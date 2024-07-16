9854f4fb964a1fda504e346369b86e3ac7c9aa78
Aula ADA M02 <aldojuniorpositivo@gmail.com>
2024-07-16 18:06:48 -0300

    implemnetação do detalhar autor

--- src/bancoDeDados.ts
+++ src/bancoDeDados.ts
@@ -2,5 +2,6 @@ import Autor from "../models/autor";
 import Postagem from "./postagem";
 
 export const autores: Autor[] = []
+
 export const posts: Postagem[] = []
 
--- src/router.ts
+++ src/router.ts
@@ -6,5 +6,6 @@ const rotas = Router()
 const autorContraldor = new AutorControlador()
 
 rotas.get('/autores', autorContraldor.listar)
+rotas.get('/autores/:id', autorContraldor.detalhar)
 
 export default rotas
