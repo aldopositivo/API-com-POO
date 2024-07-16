bbf5d4a9feed8c84c63522332adb3ed78d86eadc
Aula ADA M02 <aldojuniorpositivo@gmail.com>
2024-07-16 18:56:08 -0300

    implemnetação do cadastrar autor

--- src/router.ts
+++ src/router.ts
@@ -7,5 +7,6 @@ const autorContraldor = new AutorControlador()
 
 rotas.get('/autores', autorContraldor.listar)
 rotas.get('/autores/:id', autorContraldor.detalhar)
+rotas.post('/cadastrar',autorContraldor.cadastrar)
 
 export default rotas
