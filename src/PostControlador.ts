788941b09e9a6921176993e25e1b8b8149e14179
Aula ADA M02 <aldojuniorpositivo@gmail.com>
2024-07-16 21:23:39 -0300

    implemnetação do cadastrar postagens

--- src/PostControlador.ts
+++ src/PostControlador.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { posts } from "./bancoDeDados";
+import { autores, posts } from "./bancoDeDados";
+import Postagem from "./postagem";
 
 
 export default class PostControlador {
@@ -24,7 +25,32 @@ export default class PostControlador {
     }
 
     cadastrarPost(req: Request, res: Response){
-    
+        const { titulo, descricao, autor_id} = req.body
+
+        if(!titulo||!descricao||!autor_id) {
+            return res.send(400).json({
+                mensagem: "Todos os campos são obrigatórios!"
+            })
+        }
+
+        const autor = autores.find((elemento) => {
+            return elemento.id === autor_id
+        })
+        if (!autor) {
+            return res.status(404).json({
+                mensagem: "Autor não encontrado(a)!"
+            })
+        }
+
+        const post = new Postagem({
+            titulo,
+            descricao,
+            autor
+        })
+
+        posts.push(post)
+        
+        return res.status(201).json(post)
     }
 
     editarPost(req: Request, res: Response){
